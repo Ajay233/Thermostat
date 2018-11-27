bbdf698f2bdf2fe7e865a562f9809ed2bc97cda6
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-27 16:34:46 +0000

    implements check for energy usage

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,6 +3,7 @@ function Thermostat(){
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
   this.DEFAULT_TEMPERATURE = 20;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
   this.temperature = this.DEFAULT_TEMPERATURE;
   this.powerSavingMode = true;
 }
@@ -50,4 +51,14 @@ Thermostat.prototype.isMaxumumTemperature = function () {
 
 Thermostat.prototype.resetTemperature = function() {
   this.temperature = this.DEFAULT_TEMPERATURE;
+}
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
 }
